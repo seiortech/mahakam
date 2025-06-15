4526a5ebf8f00b122ce3c982fbd9b6373e795651
Raden Mohamad Rishwan <radenrishwan@gmail.com>
2025-06-15 19:10:31 +0700

    add doc and some utility function

--- websocket/extensions/room.go
+++ websocket/extensions/room.go
@@ -7,11 +7,13 @@ import (
 	"github.com/seiortech/mahakam/websocket"
 )
 
+// Message represents a message sent in the room.
 type Message struct {
 	Data   []byte
 	Client *websocket.Client
 }
 
+// RoomOption contains options for configuring the behavior of a Room.
 type RoomOption struct {
 	RestrictedBroadcast bool
 	OnError             func(error)
@@ -20,6 +22,7 @@ type RoomOption struct {
 	OnMessage           func(Message)
 }
 
+// Room represents a chat room that manages clients and broadcasts messages.
 type Room struct {
 	Name    string
 	clients map[*websocket.Client]bool
@@ -30,12 +33,19 @@ type Room struct {
 	Mutex   sync.RWMutex
 }
 
+// NewRoom creates a new Room with the specified name and options.
 func NewRoom(name string, option *RoomOption) Room {
 	if option == nil {
 		option = &RoomOption{
 			RestrictedBroadcast: false,
 			OnError: func(err error) {
 				log.Println(err)
+			},
+			OnEnter: func(msg Message) {
+			},
+			OnLeave: func(msg Message) {
+			},
+			OnMessage: func(msg Message) {
 			},
 		}
 	}
@@ -51,6 +61,7 @@ func NewRoom(name string, option *RoomOption) Room {
 	}
 }
 
+// Run starts the room's event loop, processing enter, leave, and message events.
 func (r *Room) Run() {
 	for {
 		select {
@@ -69,6 +80,7 @@ func (r *Room) Run() {
 	}
 }
 
+// Add adds a client to the room and broadcasts the enter message.
 func (r *Room) Add(client *websocket.Client) {
 	r.Mutex.Lock()
 	defer r.Mutex.Unlock()
@@ -76,6 +88,7 @@ func (r *Room) Add(client *websocket.Client) {
 	r.clients[client] = true
 }
 
+// Remove removes a client from the room and broadcasts the leave message.
 func (r *Room) Remove(client *websocket.Client) {
 	r.Mutex.Lock()
 	defer r.Mutex.Unlock()
@@ -83,6 +96,7 @@ func (r *Room) Remove(client *websocket.Client) {
 	delete(r.clients, client)
 }
 
+// Close closes the room, disconnecting all clients, closing channels, and sending a leave message with normal closure code.
 func (r *Room) Close() {
 	r.Mutex.Lock()
 	defer r.Mutex.Unlock()
@@ -101,6 +115,7 @@ func (r *Room) Close() {
 	close(r.Message)
 }
 
+// Broadcast sends a message to all clients in the room.
 func (r *Room) Broadcast(message []byte, opcode websocket.Opcode) error {
 	r.Mutex.Lock()
 	defer r.Mutex.Unlock()
@@ -119,6 +134,7 @@ func (r *Room) Broadcast(message []byte, opcode websocket.Opcode) error {
 	return nil
 }
 
+// BroadcastEnter broadcast a message and add the client to the room.
 func (r *Room) BroadcastEnter(msg []byte, client *websocket.Client) {
 	r.Enter <- Message{
 		Data:   msg,
@@ -126,6 +142,7 @@ func (r *Room) BroadcastEnter(msg []byte, client *websocket.Client) {
 	}
 }
 
+// BroadcastLeave broadcast a message and remove the client from the room.
 func (r *Room) BroadcastLeave(msg []byte, client *websocket.Client) {
 	r.Leave <- Message{
 		Data:   msg,
@@ -133,9 +150,39 @@ func (r *Room) BroadcastLeave(msg []byte, client *websocket.Client) {
 	}
 }
 
+// BroadcastMessage broadcasts a message to all clients in the room.
 func (r *Room) BroadcastMessage(msg []byte, client *websocket.Client) {
 	r.Message <- Message{
 		Data:   msg,
 		Client: client,
 	}
+}
+
+// Count returns the number of clients currently in the room.
+func (r *Room) Count() int {
+	r.Mutex.RLock()
+	defer r.Mutex.RUnlock()
+
+	return len(r.clients)
+}
+
+// Has checks if a client is in the room.
+func (r *Room) Has(client *websocket.Client) bool {
+	r.Mutex.RLock()
+	defer r.Mutex.RUnlock()
+
+	_, exists := r.clients[client]
+	return exists
+}
+
+// Clients returns a slice of all clients currently in the room.
+func (r *Room) Clients() []*websocket.Client {
+	r.Mutex.RLock()
+	defer r.Mutex.RUnlock()
+
+	clients := make([]*websocket.Client, 0, len(r.clients))
+	for client := range r.clients {
+		clients = append(clients, client)
+	}
+	return clients
 }
